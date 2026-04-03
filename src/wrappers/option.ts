ab45cb1430d673ecf832e44f99e5eadb931ae2d9
edho08 <edho08@gmail.com>
2026-04-03 08:51:33 +0700

    fix: rewrite Option with proper monomorphization pattern
    - Option.for(Position) returns a subclass of Option<Position>
    - Static methods (make, some, none) on base Option class
    - Memoized per component constructor
    - Different components get different classes at runtime

--- src/wrappers/option.ts
+++ src/wrappers/option.ts
@@ -1,59 +1,52 @@
 import { Component, ComponentCtor } from '../entity/component';
 
-export interface Option<C extends Component> {
-  readonly value: C | undefined;
-  isSome(): boolean;
-  isNone(): boolean;
-  unwrap(): C;
-}
+class Option<C> {
+  constructor(protected value: C | undefined) {}
 
-export interface OptionClass<C extends Component> {
-  new (value: C | undefined): Option<C>;
-  make(value: C): Option<C>;
-  some(value: C): Option<C>;
-  none(): Option<C>;
-}
+  isSome(): boolean {
+    return this.value !== undefined;
+  }
 
-const optionCache = new Map<ComponentCtor, OptionClass<any>>();
+  isNone(): boolean {
+    return this.value === undefined;
+  }
 
-export const Option = {
-  for<C extends Component>(ctor: ComponentCtor): OptionClass<C> {
-    if (optionCache.has(ctor)) {
-      return optionCache.get(ctor) as OptionClass<C>;
+  unwrap(): C {
+    if (this.value === undefined) {
+      throw new TypeError('Cannot unwrap None');
+    }
+    return this.value;
+  }
+
+  static make<C>(value: C): Option<C> {
+    return new this(value) as Option<C>;
+  }
+
+  static some<C>(value: C): Option<C> {
+    return new this(value) as Option<C>;
+  }
+
+  static none<C>(): Option<C> {
+    return new this(undefined) as Option<C>;
+  }
+
+  private static cache = new Map<ComponentCtor, new (value: any) => Option<any>>();
+
+  static for<C extends Component>(ctor: new () => C): new (value: C | undefined) => Option<C> {
+    const cached = Option.cache.get(ctor);
+    if (cached !== undefined) {
+      return cached as new (value: C | undefined) => Option<C>;
     }
 
-    class OptionInstance implements Option<C> {
-      constructor(public readonly value: C | undefined) {}
-
-      isSome(): boolean {
-        return this.value !== undefined;
+    const MonoOption = class extends Option<C> {
+      constructor(value: C | undefined) {
+        super(value);
       }
+    };
 
-      isNone(): boolean {
-        return this.value === undefined;
-      }
+    Option.cache.set(ctor, MonoOption);
+    return MonoOption as new (value: C | undefined) => Option<C>;
+  }
+}
 
-      unwrap(): C {
-        if (this.value === undefined) {
-          throw new TypeError('Cannot unwrap None');
-        }
-        return this.value;
-      }
-
-      static make(value: C): OptionInstance {
-        return new OptionInstance(value);
-      }
-
-      static some(value: C): OptionInstance {
-        return new OptionInstance(value);
-      }
-
-      static none(): OptionInstance {
-        return new OptionInstance(undefined);
-      }
-    }
-
-    optionCache.set(ctor, OptionInstance as OptionClass<C>);
-    return OptionInstance as OptionClass<C>;
-  },
-};
+export { Option };
